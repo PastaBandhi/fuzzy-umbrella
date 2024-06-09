2841b1b7fb698961965432d9ab780ed806e8cdbb
PastaBandhi <krissy.koller98@gmail.com>
2024-06-09 18:21:38 +0200

    change

---
+++ KrissyVRRoom/Assets/_Course Library/Scripts/Interaction/InteractableObject.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour
+{
+    public GameObject exclamationMark;
+
+    void Start()
+    {
+        if (exclamationMark != null)
+        {
+            exclamationMark.SetActive(false); // Ausrufezeichen zunächst verstecken
+        }
+        else
+        {
+            Debug.Log("Not assigned");
+        }
+    }
+
+    void OnMouseOver()
+    {
+        Debug.Log("Mouse is over the object.");
+        if (exclamationMark != null)
+        {
+            exclamationMark.SetActive(true); // Ausrufezeichen anzeigen, wenn der Mauszeiger über dem Objekt ist
+        }
+        else
+        {
+            Debug.Log("Something wrong");
+        }
+    }
+
+    void OnMouseExit()
+    {
+        Debug.Log("Mouse exited the object.");
+        if (exclamationMark != null)
+        {
+            exclamationMark.SetActive(false); // Ausrufezeichen verstecken, wenn der Mauszeiger das Objekt verlässt
+        }
+        else
+        {
+            Debug.Log("Something wrong");
+        }
+    }
+}
